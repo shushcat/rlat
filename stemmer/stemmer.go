559cfab76ff9d1d264a49b9ad85bfbf1f416ffec
J. O. Brickley <me@jobrickley.com>
2021-09-04 23:35:05 -0800

    Add stemmer_test.go

--- stemmer/stemmer.go
+++ stemmer/stemmer.go
@@ -2,6 +2,7 @@ package stemmer
 
 // TODO: Implement or include a Porter stemmer.
 
-func Stem(word string) string {
-	return word
+func Stem(word string) (string, string) {
+	wordstem := word
+	return wordstem, word
 }
--- stemmer/stemmer_test.go
+++ stemmer/stemmer_test.go
@@ -1,12 +1,23 @@
 package stemmer
 
-import (
-	"testing"
-)
+import "testing"
 
-func TestStemmerPresent (t *testing.T) {
-	word := "addressable"
-	if !(stem(word) == "address") {
-		t.Errorf("stemming '%v' did not yield 'address'", word)
+func TestStemmer(t *testing.T) {
+	var wordstem, word string
+	wordstem, word = Stem("addressable")
+	if wordstem != "address" {
+		t.Errorf("stemming '%v' yielded '%v', not 'address'", word, wordstem)
+	}
+	wordstem, word = Stem("loving")
+	if wordstem != "love" {
+		t.Errorf("stemming '%v' yielded '%v', not 'love'", word, wordstem)
+	}
+	wordstem, word = Stem("Argumentative")
+	if wordstem != "argument" {
+		t.Errorf("stemming '%v' yielded '%v', not 'argument'", word, wordstem)
+	}
+	wordstem, word = Stem("astonishment")
+	if wordstem != "astonish" {
+		t.Errorf("stemming '%v' yielded '%v', not 'astonish'", word, wordstem)
 	}
 }
